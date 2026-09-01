2a45b8c434459220e2e3437ed8d81ca4f35c6c41
Riya <riyasmps21@gmail.com>
2026-09-01 23:24:17 +0530

    Time: 14 ms (22.63%), Space: 62.8 MB (17.77%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -4,16 +4,13 @@ class Solution {
 
         ListNode temp = head;
 
-        // Store all values in ArrayList
         while (temp != null) {
             arr.add(temp.val);
             temp = temp.next;
         }
 
-        // Sort the ArrayList
         Collections.sort(arr);
 
-        // Put sorted values back into linked list
         temp = head;
 
         for (int i = 0; i < arr.size(); i++) {
